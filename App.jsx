e4ce04e20912a1d63a8ec0f9a5a19b3301c85f16
Lindokuhle Shweni <127991096+Shweni@users.noreply.github.com>
2023-11-02 10:44:17 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+
+export default function App() {
+    return (
+        <>
+        <Navbar />
+            <Main />
+        </>
+            
+    )
+}
